22c4c766fa426206dc7e8c3eb059b2e88874d5d5
keelbeelveel <k.belveal17@gmail.com>
2022-12-12 16:45:38 -0500

    working

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-#!/env/python3
+#!env/bin/python3
 
 import sys
 import os
 import json
 import discord
 import interactions
+import requests
 from dotenv import load_dotenv
 
 bot = None
 token = None
+
+e7_url = "https://epic7x.com/character/"
 
 def init_env():
     """Load the .env file and return the token"""
@@ -18,6 +21,15 @@ def init_env():
         raise Exception("No token found in .env file")
     return token
 
+def get_e7_data(character_name):
+    """Get the data from the e7 website"""
+    url = e7_url + character_name.lower().replace(" ", "-") + "/"
+    print(url)
+    r = requests.get(url)
+    print(r.status_code)
+    if r.status_code != 200:
+        raise Exception("Error getting data from e7 website")
+    return r.text
 
 
 if __name__ == "__main__":
@@ -27,6 +39,26 @@ if __name__ == "__main__":
 @bot.command(name="ping", description="Pong!")
 async def ping(ctx: interactions.CommandContext):
     await ctx.send("Pong!")
+
+@bot.command(
+    name="test",
+    description="Test command",
+    options = [
+        interactions.Option(
+            name="character",
+            description="What you want to say",
+            type=interactions.OptionType.STRING,
+            required=True,
+        ),
+    ],
+)
+async def test(ctx: interactions.CommandContext, character: str):
+    try:
+        data = get_e7_data(character)
+        await ctx.send(f"Done! {character}")
+    except Exception as e:
+        await ctx.send(f"Hmmm... Something went wrong: {e}")
+
 
 if __name__ == "__main__":
     bot.start()
