a71515d4d1dcb16e7162cc19b4a3f42f824ad9d1
keelbeelveel <k.belveal17@gmail.com>
2022-12-12 19:34:44 -0500

    working image parse

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import datetime
 import json
 import discord
 import interactions
@@ -21,7 +22,7 @@ def init_env():
         raise Exception("No token found in .env file")
     return token
 
-def get_e7_data(character_name):
+def get_e7_character_data(character_name):
     """Get the data from the e7 website"""
     url = e7_url + character_name.lower().replace(" ", "-") + "/"
     print(url)
@@ -29,8 +30,28 @@ def get_e7_data(character_name):
     print(r.status_code)
     if r.status_code != 200:
         raise Exception("Error getting data from e7 website")
-    return r.text
+    return r.text, url
 
+def parse_character_data(data, url, character_name):
+    """Parse the data and return the character info"""
+    lines = data.splitlines()
+
+    # Get the image link -- Find line containing this string
+    image_line = [line for line in lines if "var SELECTED_SKIN =" in line][0]
+    image_link = image_line.split('=')[1]
+    for _ in ["'", '"', ";"]:
+        image_link = image_link.replace(_, "")
+    image_link.strip()
+
+    return interactions.Embed(
+        title=f"{character_name.title()}",
+        description=f"{character_name}",
+        color=0xd700ff,
+        timestamp=datetime.datetime.utcnow(),
+        thumbnail=interactions.EmbedImageStruct(url=image_link),
+        url=url
+        #provider=interactions.EmbedProvider(name="Epic Seven Wiki", url="https://epic7x.com/")
+    )
 
 if __name__ == "__main__":
     token = init_env()
@@ -41,27 +62,26 @@ async def ping(ctx: interactions.CommandContext):
     await ctx.send("Pong!")
 
 @bot.command(
-    name="test",
-    description="Test command",
+    name="get_character",
+    description="Get the character info from the Epic Seven Wiki.",
     options = [
         interactions.Option(
             name="character",
-            description="What you want to say",
+            description="The Character to search for.",
             type=interactions.OptionType.STRING,
             required=True,
         ),
     ],
 )
-async def test(ctx: interactions.CommandContext, character: str):
+async def get_character(ctx: interactions.CommandContext, character: str):
     try:
-        data = get_e7_data(character)
-        await ctx.send(f"Done! {character}")
+        character = character.lower()
+        message = await ctx.send("Getting data...")
+        data, url = get_e7_character_data(character)
+        await message.edit(content=None, embeds=parse_character_data(data, url, character))
     except Exception as e:
-        await ctx.send(f"Hmmm... Something went wrong: {e}")
+        await message.edit(f"Hmmm... Something went wrong: {e}")
 
 
 if __name__ == "__main__":
     bot.start()
-    
-
-
